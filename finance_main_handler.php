9a2dd84fc3990359a248fbceac8b7144849cb3e5
Jonathan Carter <jonathan.carter@divido.com>
2019-03-27 13:39:25 +0000

    fix spelling

--- finance_main_handler.php
+++ finance_main_handler.php
@@ -78,10 +78,10 @@ if (isset($_GET['type']) && $_GET['type'] == 'financepayment' && isset($_GET['re
   if($order_id && $status) {
     if ($current_order_state['orders_status_id'] != MODULE_PAYMENT_FINANCEPAYMENT_AWAITING_STATUS) {
         if ($status != $current_order_state['orders_status_id']) {
-            $finance->updateOrderStatus($order_id,$status,'Finance Apllication Id: '.$result['transaction_id']);
+            $finance->updateOrderStatus($order_id,$status,'Finance Application Id: '.$result['transaction_id']);
         }
     } elseif ($status != $current_order_state['orders_status_id']) {
-        $finance->updateOrderStatus($order_id,$status,'Finance Apllication Id: '.$result['transaction_id']);
+        $finance->updateOrderStatus($order_id,$status,'Finance Application Id: '.$result['transaction_id']);
     }
   }
 } elseif (isset($_GET['type']) && $_GET['type'] == 'financepayment' && isset($_GET['confirmation']) && isset($_GET['cartID']) !='') {
