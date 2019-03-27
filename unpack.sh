6af93f7169d7f2041d40cf1a882d098dc6503035
Freddie Line <freddie.line@divido.com>
2019-03-27 16:58:09 +0000

    update unpack

--- unpack.sh
+++ unpack.sh
@@ -1,6 +1,8 @@
 mv finance_main_handler.php catalog/
+rm -rf includes/modules/payment/financepayment
 mv includes/languages/english/modules/payment/financepayment.php catalog/includes/languages/english/modules/payment/
 mv includes/modules/payment/* catalog/includes/modules/payment/
-mv vendor catalog
+rm -rf catalog/vendor
+mv vendor catalog/vendor
 chmod 777 /var/www/oscommerce.demo.divido.com/catalog/includes/configure.php
 chmod 777 /var/www/oscommerce.demo.divido.com/catalog/admin/includes/configure.php
